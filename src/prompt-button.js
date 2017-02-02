c7b071711b754810d045dc90ed129b18d1f41bed
Jared Stark <geek@jaredstark.com>
2017-02-02 02:00:51 -0700

    move style to correct button

--- src/prompt-button.js
+++ src/prompt-button.js
@@ -13,7 +13,7 @@ class PromptButton extends Component {
                         <div className='input-group'>
                             <input ref={(input) => this.promptInput = input} type='text' className='form-control' placeholder={this.props.promptText} />
                             <span className='input-group-btn'>
-                                <button className='btn btn-primary' style={{paddingTop: '3px', paddingBottom: '3px'}} onClick={() => this.props.onConfirm(this.promptInput.value)}>OK</button>
+                                <button className='btn btn-primary' onClick={() => this.props.onConfirm(this.promptInput.value)}>OK</button>
                             </span>
                         </div>
                     </div>
@@ -21,7 +21,7 @@ class PromptButton extends Component {
                         
         return (
             <OverlayTrigger trigger='click' placement='top' overlay={denyReason}>
-                <button className={this.props.buttonStyle}>{this.props.buttonText}</button>
+                <button className={this.props.buttonStyle} style={{paddingTop: '3px', paddingBottom: '3px'}}>{this.props.buttonText}</button>
             </OverlayTrigger>                               
         );        
     }
